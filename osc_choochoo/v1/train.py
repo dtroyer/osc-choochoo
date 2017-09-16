bfb3104bd28cdf1122a090d4f9ff13db4caa95d1
Dean Troyer <dtroyer@gmail.com>
2017-09-16 15:30:00 -0500

    Add whistle attribute to train, train set command

--- osc_choochoo/v1/train.py
+++ osc_choochoo/v1/train.py
@@ -18,9 +18,11 @@ import logging
 import os
 from pkg_resources import resource_filename
 
+from cliff import command
 from cliff import lister
 from cliff import show
 from osc_lib import exceptions
+from openstackclient.i18n import _
 
 
 DATA_PATH = resource_filename('osc_choochoo.v1.train', 'data/')
@@ -39,10 +41,55 @@ class TrainList(lister.Lister):
         # get list of files in DATA_PATH
         for f in os.listdir(DATA_PATH):
             if f.endswith(".txt"):
-                data.append([f.replace('.txt', '')])
+                data.append([f.replace('.txt', ''), True])
 
-        columns = ("Name", )
+        columns = ("Name", "Whistle")
         return (columns, data)
+
+
+class TrainSet(command.Command):
+    _description = _("Modify train properties")
+
+    auth_required = False
+    log = logging.getLogger(__name__ + '.TrainSet')
+
+    def get_parser(self, prog_name):
+        parser = super(TrainSet, self).get_parser(prog_name)
+        parser.add_argument(
+            'name',
+            metavar='<train-name>',
+            help='Train to modify',
+        )
+        whistle_group = parser.add_mutually_exclusive_group()
+        whistle_group.add_argument(
+            "--disable-whistle",
+            action="store_true",
+            help=_("Disable the train whistle"),
+        )
+        whistle_group.add_argument(
+            "--enable-whistle",
+            action="store_true",
+            help=_("Enable the train whistle"),
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+
+        filename = os.path.join(DATA_PATH, parsed_args.name + '.txt')
+        try:
+            with io.open(filename) as f:
+                # Ha!  Nothing to do here, the whistle is always enabled!
+                pass
+        except IOError as e:
+            msg = "Train %(train)s not found: %(exception)s"
+            raise exceptions.CommandError(
+                msg % {
+                    "train": parsed_args.name,
+                    "exception": e,
+                }
+            )
+        return
 
 
 class TrainShow(show.ShowOne):
@@ -76,6 +123,6 @@ class TrainShow(show.ShowOne):
                 }
             )
         return (
-            ["name", "data"],
-            [parsed_args.name, ascii_art],
+            ["name", "whistle", "data"],
+            [parsed_args.name, True, ascii_art],
         )
